8853f1f1ef706da75f5e4545ec8236c1fd5dfb1b
SolomonVP <Lum7023@yandex.ru>
2022-01-05 17:24:29 +0300

    pointcut 1 (26)

--- src/main/java/com/example/spring_aop/aop/UniLibrary.java
+++ src/main/java/com/example/spring_aop/aop/UniLibrary.java
@@ -3,7 +3,8 @@ package com.example.spring_aop.aop;
 import org.springframework.stereotype.Component;
 
 @Component("libraryBean")
-public class Library {
+public class Library extends AbstractLibrary {
+    @Override
     public void getBook() {
         System.out.println("Мы берем книгу!");
     }
